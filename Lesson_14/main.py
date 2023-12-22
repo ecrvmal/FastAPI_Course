128878ecc894ce4472c4e064ccf902c7d5b10ac6
ecrvmal <ecrvmal@yandex.ru>
2023-12-22 20:17:30 +0300

    lesson14 passed

--- Lesson_14/main.py
+++ Lesson_14/main.py
@@ -12,16 +12,16 @@ async def get_async_session():
 
 
 @app.get("/items")
-async def get_items(session=Depends(get_async_session)):
-    print(session)
-    return [{"id": 1}]
+async def get_items(session=Depends(get_async_session)):      # depends от получения сессии
+    print(session)                                            # сессию получаем, перед этим открываем,
+    return [{"id": 1}]                                        # после использования закрываем
 
 
 # parameters
 def pagination_params(limit: int = 10, skip: int = 0):
     return {"limit": limit, "skip": skip}
 
-
+# при запуске программы отрабатывается что переменная pagination_params = pagination_params(), то есть вставляется dict
 @app.get("/subjects")
 async def get_subjects(pagination_params: dict = Depends(pagination_params)):
     return pagination_params
@@ -44,25 +44,34 @@ async def get_subjects_class(pagination_params: Paginator = Depends()):
 # request
 
 class AuthGuard:
-    def __init__(self, name: str):
+    def __init__(self, name: str):                           # при создании класса
         self.name = name
 
-    def __call__(self, request: Request):
+    def __call__(self, request: Request):                     # при создании экземпляра класса - проверяем авторизацию
+        # проверяем что в куках есть инфа о наличии прав пользователя
         if "super_cookie" not in request.cookies:
             raise HTTPException(status_code=403, detail="Запрещено")
-        # проверяем что в куках есть инфа о наличии прав пользователя
         return True
 
 
-auth_guard_payments = AuthGuard("payments")
+auth_guard_payments = AuthGuard("payments")      # здесь создаем  экземпляр класса
 
+# --- Variant 1 Depends  в router – для всех end-point  этого router ----
+#
+# router = APIRouter(
+#     dependencies=[Depends(auth_guard_payments)]           # - сюда экземпляр класса
+# )
 
-router = APIRouter(
-    dependencies=[Depends(auth_guard_payments)]
-)
+# ------------------ Variant 2 -----------------
+# @app.get("/payments")
+# def get_payments(auth_guard_payments: AuthGuard = Depends(auth_guard_payments)):   # здесь вызываем экземпляр класса,
+#                                                                                 # кот.проверяет авторизацию
+#     return "my payments...."
 
-
-@app.get("/payments")
-def get_payments():
+# ------------------ Variant 3 -----------------
+@app.get("/payments", dependencies=[Depends(auth_guard_payments)])           # здесь вызываем экземпляр класса,
+                                                                             # кот.проверяет авторизацию
+def get_payments():                                                          # здесь м.б. другие параметры функции,
     return "my payments...."
+
 
